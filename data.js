dfd31360abbfec5995ca9c2c40fbf4187fbba9c6
yochatapa <yochatapa@gmail.com>
2024-12-06 02:08:21 +0000

    deploy: aa98bf35599e1b3f7725f61fd68e98c5a55e7ff5

--- data.js
+++ data.js
@@ -4,37 +4,93 @@ const menuData = [
         "SN": 1,
         "ORDER_NO": 0,
         "MENU_NM": "Props"
-    }, {
+    }, 
+    /*{
         "UP_SN": 0,
         "SN": 2,
         "ORDER_NO": 0,
         "MENU_NM": "Methods"
-    }, {
-        "UP_SN": 1,
+    }, 
+    {
+        "UP_SN": 2,
         "SN": 3,
         "ORDER_NO": 0,
-        "MENU_NM": "checkbox"
-    }, {
-        "UP_SN": 1,
+        "MENU_NM": "Tree"
+    },
+    {
+        "UP_SN": 2,
         "SN": 4,
         "ORDER_NO": 0,
-        "MENU_NM": "selectable"
-    }, {
+        "MENU_NM": "Node"
+    },*/
+    // props
+    {
         "UP_SN": 1,
         "SN": 5,
         "ORDER_NO": 0,
-        "MENU_NM": "editable"
+        "MENU_NM": "checkbox",
+        "MENU_CD": "checkbox",
     }, {
         "UP_SN": 1,
         "SN": 6,
         "ORDER_NO": 0,
-        "MENU_NM": "draggable"
+        "MENU_NM": "selectable",
+        "MENU_CD": "selectable",
     }, {
         "UP_SN": 1,
         "SN": 7,
         "ORDER_NO": 0,
-        "MENU_NM": "dataset"
+        "MENU_NM": "editable",
+        "MENU_CD": "editable",
+    }, {
+        "UP_SN": 1,
+        "SN": 8,
+        "ORDER_NO": 0,
+        "MENU_NM": "draggable",
+        "MENU_CD": "draggable",
+    }, {
+        "UP_SN": 1,
+        "SN": 9,
+        "ORDER_NO": 0,
+        "MENU_NM": "dataset",
+        "MENU_CD": "dataset",
     },
+    // Tree methods
+    /*{
+        "UP_SN": 3,
+        "SN": 10,
+        "ORDER_NO": 0,
+        "MENU_NM": "expand/collapse",
+        "MENU_CD" : "tree-expand",
+    },
+    {
+        "UP_SN": 3,
+        "SN": 11,
+        "ORDER_NO": 0,
+        "MENU_NM": "data/value",
+        "MENU_CD" : "tree-data",
+    },
+    {
+        "UP_SN": 3,
+        "SN": 12,
+        "ORDER_NO": 0,
+        "MENU_NM": "getNode",
+        "MENU_CD" : "tree-getNode",
+    },
+    {
+        "UP_SN": 3,
+        "SN": 13,
+        "ORDER_NO": 0,
+        "MENU_NM": "checkNode",
+        "MENU_CD" : "tree-check",
+    },
+    {
+        "UP_SN": 3,
+        "SN": 14,
+        "ORDER_NO": 0,
+        "MENU_NM": "searchNode",
+        "MENU_CD" : "tree-search",
+    }, */
 ];
 
 const sampleData1 = [
